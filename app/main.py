50f6cd7cbf93382d49ee67b824d375f33f8ec4e8
Sangh0 <hop7311@naver.com>
2024-08-10 22:50:58 +0900

    update main file

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-import os
 import uvicorn
 
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import FileResponse
+from fastapi import FastAPI
+from pyngrok import ngrok
 
 from app.routers import train, evaluate, inference
+from config.config import Config
 
 
 app = FastAPI(
-    title="MNIST Training API",
+    title="Tutorial API",
     version="0.1",
     description="Tutorial API for training models such as CNN, CNN with BatchNorm, and MLP on various datasets such as MNIST, FashionMNIST, CIFAR10, and CIFAR100.",
 )
@@ -26,28 +26,22 @@ def read_root():
     }
 
 
-@app.get("/model_list")
-def list_models():
-    try:
-        model_registry = "./runs/train"
-        if not os.path.exists(model_registry):
-            return {"models": []}
-        models = os.listdir(model_registry)
-        return {"models": models}
+@app.post("/get_mlflow_ui_url")
+def get_mlflow_ui_url():
+    ngrok.kill()
 
-    except Exception as e:
-        raise HTTPException(status_code=501, detail=str(e))
+    NGROK_AUTH_TOKEN = Config.AUTH_TOKEN
+    ngrok.set_auth_token(NGROK_AUTH_TOKEN)
+
+    ngrok_tunnel = ngrok.connect(addr="5000", proto="http", bind_tls=True)
+    return {"MLflow Tracking UI": ngrok_tunnel.public_url}
 
 
-@app.get("/download_model")
-def download_model(exp_name: str):
-    model_registry = "./runs/train"
-    weight_file_path = f"{model_registry}/{exp_name}/best_model.pth"
-    if os.path.exists(weight_file_path):
-        return FileResponse(weight_file_path, filename="best_model.pth")
-    else:
-        raise HTTPException(status_code=404, detail="Model file not found")
+def main():
+    print(f"Authentication Key: {Config.AUTH_TOKEN}")
+    print(f"Data Directory: {Config.DATA_DIR}")
 
 
 if __name__ == "__main__":
+    main()
     uvicorn.run(app, host="0.0.0.0", port=8000)
